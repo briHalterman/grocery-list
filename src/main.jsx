1f1ec0b28a1e156d34960b63e89f6f4e630c4a10
Brittany Halterman <halterma@gmail.com>
2025-03-03 10:14:26 -0500

    Add to JSX

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,26 @@ const desc =
 const myTitleId = 'main-title';
 const name = 'Brittany';
 
+const heading = 'The Rules of JSX';
+const list = (
+  <>
+    <li>Return a single root element.</li>
+    <li>Close all the tags!</li>
+    <li>camelCase most of the things.</li>
+  </>
+);
+
 const element = (
   <>
     <h1 id={myTitleId}>{name}'s First React Elemement</h1>
     <p className="main-desc">{desc}</p>
     <input value={10 * 20} />
+    <h2><strong>{heading}</strong></h2>
+    <ol>{list}</ol>
   </>
 );
 
-const img = <img src="imapge.jpg" />;
+const img = <img src="image.jpg" />;
 
 const root = createRoot(document.getElementById('root'));
 root.render(element);
