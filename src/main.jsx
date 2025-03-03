a1636932739fd0e056e64aff5ca939653ce29a2a
Brittany Halterman <halterma@gmail.com>
2025-03-03 09:54:39 -0500

    Embed JavaScript Expressions in JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,19 @@
 import { createRoot } from 'react-dom/client';
 
-const element = <h1>My First React Element</h1>;
+const desc =
+  'I just learned how to create a react node and render it from the DOM';
+const myTitleId = 'main-title';
+const name = 'Brittany';
+
+const element = (
+  <>
+    <h1 id={myTitleId}>{name}'s First React Elemement</h1>
+    <p className="main-desc">{desc}</p>
+    <input value={10 * 20} />
+  </>
+);
+
+const img = <img src="imapge.jpg" />;
 
 const root = createRoot(document.getElementById('root'));
 root.render(element);
