d836468a4d7a546f4cdba1448e9d56ba6298f89a
Brittany Halterman <halterma@gmail.com>
2025-03-03 15:05:44 -0500

    Nest components

--- src/main.jsx
+++ src/main.jsx
@@ -12,15 +12,32 @@ const Item = () => {
     <div className="item">
       <button className="remove-item" />
       <span className="item-name">Apples</span>
-      <div className="quantity">
-        <span className="gty-label">QTY</span>
-        <button className="increment">+</button>
-        <button className="decrement">-</button>
-        <span className="quantity-amount">15</span>
-      </div>
+      <Counter />
+    </div>
+  );
+};
+
+const Counter = () => {
+  return (
+    <div className="quantity">
+      <span className="gty-label">QTY</span>
+      <button className="increment">+</button>
+      <button className="decrement">-</button>
+      <span className="quantity-amount">15</span>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="grocery-list">
+      <Header />
+
+      {/* Grocery List */}
+      <Item />
     </div>
   );
 };
 
 const root = createRoot(document.getElementById('root'));
-root.render(<Item />);
+root.render(<App />);
