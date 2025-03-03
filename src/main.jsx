0745c5811efda920135f007d26ceebc32d24a371
Brittany Halterman <halterma@gmail.com>
2025-03-03 14:16:32 -0500

    Create a component

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,11 @@
 import { createRoot } from 'react-dom/client';
 
-const desc =
-  'I just learned how to create a react node and render it from the DOM';
-const myTitleId = 'main-title';
-const name = 'Brittany';
-
-const heading = 'The Rules of JSX';
-const list = (
-  <>
-    <li>Return a single root element.</li>
-    <li>Close all the tags!</li>
-    <li>camelCase most of the things.</li>
-  </>
+const Header = () => (
+  <header>
+    <h1>Grocery List</h1>
+    <span className="total-items">Items: 1</span>
+  </header>
 );
-
-const element = (
-  <>
-    <h1 id={myTitleId}>{name}'s First React Elemement</h1>
-    <p className="main-desc">{desc}</p>
-    <input value={10 * 20} />
-    <h2><strong>{heading}</strong></h2>
-    <ol>{list}</ol>
-  </>
-);
-
-const img = <img src="image.jpg" />;
 
 const root = createRoot(document.getElementById('root'));
 root.render(element);
