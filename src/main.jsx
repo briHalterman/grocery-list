d066c17ab1cd095c078b746d2cb1aad5b7d5b7b7
Brittany Halterman <halterma@gmail.com>
2025-03-04 13:35:02 -0500

    Updating state

--- src/main.jsx
+++ src/main.jsx
@@ -48,14 +48,24 @@ const Counter = (props) => {
   const [quantity, setQuantity] = useState(0);
 
   const incrementQuantity = () => {
-    console.log("Hello from inside incrementQuantity")
-  }
+    setQuantity(quantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
 
   return (
     <div className="quantity">
       <span className="gty-label">QTY</span>
-      <button className="increment" onClick={incrementQuantity}>+</button>
-      <button className="decrement">-</button>
+      <button className="increment" onClick={incrementQuantity}>
+        +
+      </button>
+      <button className="decrement" onClick={decrementQuantity}>
+        -
+      </button>
       <span className="quantity-amount">{quantity}</span>
     </div>
   );
